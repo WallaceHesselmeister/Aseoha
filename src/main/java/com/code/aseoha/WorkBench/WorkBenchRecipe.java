14c70dc4931dfd3ff4509f7d47ec5be8fb791e3b
wallace <wallace@beansfamily.org>
2024-10-25 02:16:13 -0400

    (Hopefully) Working workbench JEI Compat

--- src/main/java/com/code/aseoha/WorkBench/WorkBenchRecipe.java
+++ src/main/java/com/code/aseoha/WorkBench/WorkBenchRecipe.java
@@ -1,13 +1,25 @@
 package com.code.aseoha.WorkBench;
 
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WorkBenchRecipe {
     Item Ingredients[] = new Item[4];
     Item Result;
 
     /**
-     * Specifies 4 ingredients, if you want only say, 2 Ingredients, leave the rest null
+     * Specifies 4 ingredients, if you want only say, 2 Ingredients, leave the rest as Items.AIR
      * @param FirstIngredient 1st Ingredient
      * @param SecondIngredient 2nd Ingredient
      * @param ThirdIngredient 3rd Ingredient
@@ -22,5 +34,31 @@ public class WorkBenchRecipe {
     public WorkBenchRecipe AddReceivingItem(Item Received){
         this.Result = Received;
         return this;
+    }
+
+    public WorkBenchRecipe Get(){
+        return this;
+    }
+
+    public List<Item> GetList(){
+        return Arrays.asList(this.Ingredients);
+    }
+
+    public List<Ingredient> GetIngredientList(){
+        Ingredient ingredient1 = Ingredient.of(this.Ingredients[0].asItem());
+        Ingredient ingredient2 = Ingredient.of(this.Ingredients[1].asItem());
+        Ingredient ingredient3 = Ingredient.of(this.Ingredients[2].asItem());
+        Ingredient ingredient4 = Ingredient.of(this.Ingredients[3].asItem());
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(ingredient1);
+        ingredients.add(ingredient2);
+        ingredients.add(ingredient3);
+        ingredients.add(ingredient4);
+        return ingredients;
+//        return
+    }
+
+    public Item GetOutput(){
+        return this.Result;
     }
 }
