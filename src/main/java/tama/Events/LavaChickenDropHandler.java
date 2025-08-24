2e31bd99e0544c3c99ae8a5465866170261fae99
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2025-08-23 23:23:03 -0500

    add drop handler and fix missing texture and sound data

---
+++ src/main/java/tama/Events/LavaChickenDropHandler.java
@@ -0,0 +1,29 @@
+package tama.Events;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.item.ItemEntity;
+import tama.Items.AItems;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class LavaChickenDropHandler {
+    @SubscribeEvent
+    public static void onLivingDrops(LivingDropsEvent event) {
+        LivingEntity entity = event.getEntityLiving();
+        if (entity instanceof Zombie) {
+            Zombie zombie = (Zombie) entity;
+            if (zombie.isBaby() && zombie.getVehicle() instanceof Chicken) {
+                Level world = zombie.level;
+                ItemStack dropStack = new ItemStack(AItems.LAVA_CHICKEN_DISC.get());
+                ItemEntity itemEntity = new ItemEntity(world, zombie.getX(), zombie.getY(), zombie.getZ(), dropStack);
+                event.getDrops().add(itemEntity);
+            }
+        }
+    }
+}
